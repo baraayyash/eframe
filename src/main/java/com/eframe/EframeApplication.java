c257dab920f1231349a503e7de1118a831063196
baraayyash <baraayyash@macs-MacBook-Pro.local>
2017-06-09 16:08:08 +0300

    first commit

---
+++ src/main/java/com/eframe/EframeApplication.java
@@ -0,0 +1,12 @@
+package com.eframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EframeApplication.class, args);
+	}
+}
